6bf839d1d8635f256545b2b3af07b42146b94cf6
Victor Estevez Rodriguez <asaji.judo@gmail.com>
2022-10-05 10:52:58 -0400

    Prueba mas judokas

--- Main.java
+++ Main.java
@@ -6,7 +6,13 @@ import static LigaModel.Judoka.Kyu.AMARILLO;
 
 public class Main {
     public static void main(String[] args) {
+
         Judoka j1 = new Judoka("Luis", "Hinojosa", "2345", "234567","algo@der.com", "villa", (byte) 0, "-66",68,new Date(2000, 03, 24), AMARILLO, new Date(2012, 06,14));
+        Judoka j2 = new Judoka("Juan", "Perez", "2345", "234567","algo@der.com", "villa", (byte) 0, "-66",68,new Date(2000, 03, 24), AMARILLO, new Date(2012, 06,14));
+        Judoka j3= new Judoka("Marco", "Soliz", "2345", "234567","algo@der.com", "villa", (byte) 0, "-66",68,new Date(2000, 03, 24), AMARILLO, new Date(2012, 06,14));
+        Judoka j4 = new Judoka("Marcelo", "Tapia", "2345", "234567","algo@der.com", "villa", (byte) 0, "-66",68,new Date(2000, 03, 24), AMARILLO, new Date(2012, 06,14));
+        Judoka j5 = new Judoka("Diego", "Gtierrez", "2345", "234567","algo@der.com", "villa", (byte) 0, "-66",68,new Date(2000, 03, 24), AMARILLO, new Date(2012, 06,14));
+        Judoka j6 = new Judoka("Andres", "Aragon", "2345", "234567","algo@der.com", "villa", (byte) 0, "-66",68,new Date(2000, 03, 24), AMARILLO, new Date(2012, 06,14));
 
     }
 }
